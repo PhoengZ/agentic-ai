b1ca78ffb7f8c723fd503b455ee420ee25ceae4b
Phaolap Kulteera <pholapcondo11@gmail.com>
2026-08-01 19:16:23 +0700

    Update Workflow and main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from scripts.workflow import graph
+if __name__ == "__main__":
+    print("This is agent to answer the question about policy of Company A. If you want to end the session, please type 'exit'.")
+    print("______________________________________________________________")
+    while(True):
+        question = input("Please ask your question : ")
+        if question == "exit":
+            break
+        init_state = {
+            "question" : question,
+            "documents" : [],
+            "answer" : "",
+            "next_node" : "supervisor_agent"
+        }
+        answer = graph.invoke(init_state, config={"recursion_limit": 10})
+        print("=========== Answer ===========")
+        print(answer['answer'])
---
+++ scripts/workflow.py
@@ -0,0 +1,25 @@
+from langgraph.graph import StateGraph, END
+from state_graph import GraphState
+from agents import generator_agent_node, retrieve_agent_node, supervisor_agent_node, route_logic
+workflow = StateGraph(GraphState)
+
+workflow.add_node("generator_agent", generator_agent_node)
+workflow.add_node("retriever_agent", retrieve_agent_node)
+workflow.add_node("supervisor_agent", supervisor_agent_node)
+
+workflow.set_entry_point("supervisor_agent")
+
+workflow.add_edge("generator_agent", "supervisor_agent")
+workflow.add_edge("retriever_agent", "supervisor_agent")
+
+workflow.add_conditional_edges(
+    "supervisor_agent",
+    route_logic,
+    {
+        "generator_agent": "generator_agent",
+        "retriever_agent": "retriever_agent",
+        "__end__": END
+    }
+)
+
+graph = workflow.compile()
